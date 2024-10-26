b55382aced1c482985e6c357d1922888ff993405
Alcides <acremonezi@gmail.com>
2024-10-26 09:23:32 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.showString(" HAPPY HALLOWEEN VICTOR")
+    pins.digitalWritePin(DigitalPin.P13, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+})
